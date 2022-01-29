e95ad5bf5759faaf3b36912f58345d9752aba2d8
DongHun, Kim <donghun.kdh@gmail.com>
2022-01-29 19:26:09 +0900

    revise route

---
+++ App/pages/search/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <HeadLine />
+    <Search />
+    <!-- <FoodList /> -->
+  </div>
+</template>
+
+<script>
+  import HeadLine from '~/components/Search/HeadLine';
+  import Search from '~/components/Search/Search';
+  // import FoodList from '~/components/Search/FoodList';
+
+  export default {
+    components: {
+      Search,
+      HeadLine,
+      // FoodList
+    },
+    //  created() {
+    //   this.$store.commit('food/resetFood')
+    // }
+  };
+</script>
+
+<style lang="scss" scoped></style>
