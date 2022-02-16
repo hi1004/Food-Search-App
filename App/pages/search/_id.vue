6320f9b49ac553939346e8ebd336c03423aaa4ea
DongHun, Kim <donghun.kdh@gmail.com>
2022-02-16 19:23:48 +0900

    revise chart

--- App/pages/search/_id.vue
+++ App/pages/search/_id.vue
@@ -173,33 +173,65 @@
       // Create Chart Method
       createChart(chartId) {
         // set default
-        let carbohydrate = 666;
-        let protein = 666;
-        let fat = 666;
+        let kcal = 0;
+        let carbohydrate = 0;
+        let protein = 0;
+        let fat = 0;
+        let transFat = 0;
+        let saturatedFat = 0;
+        let cholesterol = 0;
+        let natrium = 0;
+        let sugars = 0;
+        let calcium = 0;
+        let iron = 0;
         // parsing nutrient data if nutrient data is not unknown
         const nutrient = this.theFood.nutrient;
-        if (nutrient != '알수없음') {
-          carbohydrate = Number(nutrient.match(/탄수화물 [0-9.]*/)[0].replace('탄수화물 ', ''));
-          protein = Number(nutrient.match(/단백질 [0-9.]*/)[0].replace('단백질 ', ''));
-          fat = Number(nutrient.match(/지방 [0-9.]*/)[0].replace('지방 ', ''));
+        if (nutrient != '알수없음' && nutrient != '알 수 없음' && nutrient) {
+          kcal = nutrient.match(/열량 [0-9.]*/) ? Number(nutrient.match(/열량 [0-9.]*/)[0].replace('열량 ', '')) : 0;
+          carbohydrate = nutrient.match(/탄수화물 [0-9.]*/) ? Number(nutrient.match(/탄수화물 [0-9.]*/)[0].replace('탄수화물 ', '')) : 0;
+          protein = nutrient.match(/단백질 [0-9.]*/) ? Number(nutrient.match(/단백질 [0-9.]*/)[0].replace('단백질 ', '')) : 0;
+          fat = nutrient.match(/지방 [0-9.]*/) ? Number(nutrient.match(/지방 [0-9.]*/)[0].replace('지방 ', '')) : 0;
+          transFat = nutrient.match(/트랜스지방 [0-9.]*/) ? Number(nutrient.match(/트랜스지방 [0-9.]*/)[0].replace('트랜스지방 ', '')) : 0;
+          saturatedFat = nutrient.match(/포화지방 [0-9.]*/) ? Number(nutrient.match(/포화지방 [0-9.]*/)[0].replace('포화지방 ', '')) : 0;
+          cholesterol = nutrient.match(/콜레스테롤 [0-9.]*/) ? Number(nutrient.match(/콜레스테롤 [0-9.]*/)[0].replace('콜레스테롤 ', '')) : 0;
+          natrium = nutrient.match(/나트륨 [0-9.]*/) ? Number(nutrient.match(/나트륨 [0-9.]*/)[0].replace('나트륨 ', '')) : 0;
+          sugars = nutrient.match(/당류 [0-9.]*/) ? Number(nutrient.match(/당류 [0-9.]*/)[0].replace('당류 ', '')) : 0;
+          calcium = nutrient.match(/칼슘 [0-9.]*/) ? Number(nutrient.match(/칼슘 [0-9.]*/)[0].replace('칼슘 ', '')) : 0;
+          iron = nutrient.match(/철 [0-9.]*/) ? Number(nutrient.match(/철 [0-9.]*/)[0].replace('철 ', '')) : 0;
         }
         // create chart
         const ctx = document.getElementById(chartId);
         new Chart(ctx, {
-          type: 'doughnut',
+          type: 'bar',
           data: {
-            labels: ['탄수화물', '단백질', '지방'],
+            labels: ['열량', '탄수화물', '단백질', '지방', '트랜스지방', '포화지방', '콜레스테롤', '나트륨', '당류', '칼슘', '철분'],            
             datasets: [
-              {
-                label: 'nutrients data',
-                data: [carbohydrate, protein, fat],
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
-                hoverOffset: 4,
+              {                
+                data: [
+                  kcal,
+                  carbohydrate,
+                  protein,
+                  fat,
+                  transFat,
+                  saturatedFat,
+                  cholesterol,
+                  natrium,
+                  sugars,
+                  calcium,
+                  iron,
+                ],
+                backgroundColor: [
+                ]
               },
             ],
           },
           options: {
-            responsive: false,
+            responsive: true,
+            scales: {
+              y: {
+                beginAtZero: true,
+              }
+            }
           },
         });
       },
@@ -332,6 +364,9 @@
         justify-content: center;
         align-items: center;
         background: lightpink;
+        canvas {
+          width: 100%;
+        }
       }
       @include media-breakpoint-down(xl) {
         flex-direction: column;
